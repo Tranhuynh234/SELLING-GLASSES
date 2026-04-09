02f0b04fb38cc047c172c7fd9e9c5b77e1a8c60e
quangvu2801 <vutruong6kg@gmail.com>
2026-04-10 00:01:58 +0700

    Create payment_config.php

---
+++ config/payment_config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'bankCode' => 'MB',
+    'bankName' => 'MB Bank',
+    'accountNumber' => '0123456789',
+    'accountName' => 'EYESGLASS ADMIN',
+    'branch' => 'Ho Chi Minh',
+    'transferPrefix' => 'EYESGLASS',
+    'defaultShippingFee' => 30000,
+];
